4bb1f68774f756d757c9727f6f93506335973904
Andrey Vasilyev <an.vasiliev@vk.team>
2026-09-23 20:15:50 +0300

    fix test annotations

--- src/integrationTest/java/company/vk/edu/distrib/compute/test/urlshortener/AbstractArgumentsProvider.java
+++ src/integrationTest/java/company/vk/edu/distrib/compute/test/urlshortener/AbstractArgumentsProvider.java
@@ -8,7 +8,6 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.stream.Stream;
 
-import company.vk.edu.distrib.compute.rsmt98.urlshortener.UrlShortenerServiceFactory;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.support.ParameterDeclarations;
@@ -18,9 +17,11 @@ import org.junit.platform.commons.util.ReflectionUtils;
 public class AbstractArgumentsProvider {
 
     private final Collection<Class<?>> factories;
+    private final Class<?> factoryClass;
 
-    public AbstractArgumentsProvider(Collection<Class<?>> factories) {
+    public AbstractArgumentsProvider(Collection<Class<?>> factories, Class<?> factoryClass) {
         this.factories = factories;
+        this.factoryClass = factoryClass;
     }
 
     public static Collection<Class<?>> findAnnotatedFactories(Class<? extends Annotation> annotation) {
@@ -37,10 +38,10 @@ public class AbstractArgumentsProvider {
 
     public Stream<? extends Arguments> provideArguments(ParameterDeclarations parameters, ExtensionContext context) {
         return factories.stream()
+            .filter(factoryClass::isAssignableFrom)
             .filter(it -> Arrays.stream(it.getDeclaredConstructors())
                 .anyMatch(ctor -> ctor.getParameterCount() == 0))
             .map(ReflectionUtils::newInstance)
-            .filter(it -> it instanceof UrlShortenerServiceFactory)
             .map(Arguments::of);
     }
 }
